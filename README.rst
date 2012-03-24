4cf05c093e76019a7854fcff415c99671cb6115f
Daniel Gerber <daniel.g.gerber@gmail.com>
2012-03-24 12:12:40 +0100

    add README

---
+++ README.rst
@@ -0,0 +1,3 @@
+
+
+This module provides regular expressions according to `RFC 3986 "Uniform Resource Identifier (URI): Generic Syntax" <http://tools.ietf.org/html/rfc3986>`_ and `RFC 3987 "Internationalized Resource Identifiers (IRIs)" <http://tools.ietf.org/html/rfc3987>`_.
--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 from distutils.core import setup
 from rfc3987 import __version__
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(name='rfc3987',
       version= __version__,
       description='Parsing and validation of URIs and IRIs',
-      long_description='This module provides regular expressions according to RFC 3986 "Uniform Resource Identifier (URI): Generic Syntax" and RFC 3987 "Internationalized Resource Identifiers (IRIs)".',
+      long_description=long_description,
       author='Daniel Gerber',
       url='https://gist.github.com/1333605',
       py_modules=['rfc3987'],
